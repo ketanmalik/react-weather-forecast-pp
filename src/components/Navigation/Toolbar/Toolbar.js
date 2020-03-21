1d991234f3b8ec064a561a4a7d9ff5764559c95f
ketanmalik <malik.ke@husky.neu.edu>
2020-03-20 20:23:01 -0400

    toolbar-scaffolding

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import logo from "../../../assets/images/logo.png";
+import classes from "./Toolbar.module.css";
+
+class Toolbar extends Component {
+  state = {
+    showHeading: false
+  };
+  componentDidMount() {
+    setTimeout(() => this.setState({ showHeading: true }), 10);
+  }
+
+  render() {
+    const styles = {
+      heading: {
+        marginTop: "10px",
+        opacity: this.state.showHeading ? 1 : 0,
+        transition: "opacity 2s"
+      },
+      logo: {
+        opacity: this.state.showHeading ? 1 : 0,
+        transition: "opacity 2s"
+      }
+    };
+
+    return (
+      <div className={classes.Toolbar}>
+        <img style={styles.logo} src={logo} alt="Logo" />
+        <h2 style={styles.heading}>Weather Forecast Application</h2>
+      </div>
+    );
+  }
+}
+
+export default Toolbar;
