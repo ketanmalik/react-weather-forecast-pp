804428f8673d4c5f4149f24baf35c43d2a6d52f0
ketanmalik <malik.ke@husky.neu.edu>
2020-03-21 01:51:24 -0400

    styling-ver-2

--- src/components/FiveDayView/DayBanner/DayBanner.js
+++ src/components/FiveDayView/DayBanner/DayBanner.js
@@ -1,6 +1,16 @@
 import React from "react";
 import classes from "./DayBanner.module.css";
 
-const dayBanner = props => <div className={classes.Wrapper}>Today</div>;
+const dayBanner = props => {
+  let wrapperClass = `${classes.Wrapper}`;
+  if (props.selected === props.value) {
+    wrapperClass = `${classes.SideDrawer} ${classes.Active}`;
+  }
+  return (
+    <a className={classes.Links} onClick={() => props.clicked(props.value)}>
+      <div className={wrapperClass}>{props.day}</div>
+    </a>
+  );
+};
 
 export default dayBanner;
--- src/components/FiveDayView/SelectedDay/SelectedDay.js
+++ src/components/FiveDayView/SelectedDay/SelectedDay.js
@@ -2,15 +2,18 @@ import React from "react";
 import sunny from "../../../assets/images/sunny.png";
 import classes from "./SelectedDay.module.css";
 
-const selectedDay = props => (
-  <div className={classes.Wrapper}>
-    <h1>
-      <span> {props.day} - Sunny </span>
-      <img src={sunny} alt="sunny" />
-    </h1>
-    High - 24<sup>o</sup>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Low - 18<sup>o</sup>
-  </div>
-);
+const selectedDay = props => {
+  let day = props.day.split(" ");
+  return (
+    <div className={classes.Wrapper}>
+      <h1>
+        <span> {day[1] + " " + day[2]} - Sunny </span>
+        <img src={sunny} alt="sunny" />
+      </h1>
+      High - 24<sup>o</sup>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Low - 18<sup>o</sup>
+    </div>
+  );
+};
 
 export default selectedDay;
