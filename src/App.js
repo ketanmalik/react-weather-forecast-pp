7ae352fcc1538e9dd500c7893f27697a2e086e52
ketanmalik <malik.ke@husky.neu.edu>
2020-03-28 11:19:22 -0500

    merge-error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import WeatherForecast from "./containers/WeatherForecast/WeatherForecast";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./App.css";
+import "./App.module.css";
 
 class App extends Component {
   render() {
